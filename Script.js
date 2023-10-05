53a11169c4449e26034d743fd9c0511a9d25dc83
Wesam20061 <144031435+Wesam20061@users.noreply.github.com>
2023-10-05 11:33:26 +0200

    opdracht 5.1

---
+++ Script.js
@@ -0,0 +1,11 @@
+window.onload = function () {
+   
+    var heading = document.getElementById("myHeading");
+
+    
+    heading.style.color = "blue";
+   
+
+   
+    heading.textContent = "Welkom op onze website";
+};
